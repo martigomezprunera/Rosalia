b532bfb0b6257811e5b66c175ed777dea2d5df0d
Sergio <Sergio.escuder5@gmail.com>
2020-02-11 10:45:52 +0100

    Attack Logic Works but Neds to Be Rotated

---
+++ Rosalia_game/Assets/Scripts/Attack.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Attack : MonoBehaviour
+{
+    public enum Side
+    {
+        RIGHT,
+        LEFT
+    }
+
+    [Header("TRANSFORMS")]
+    [SerializeField] Transform centreOfAttack;
+    [SerializeField] Transform rightSpawner;
+    [SerializeField] Transform leftSpawner;
+    [SerializeField] PlayerMovement myPlayer;
+    
+    [Header("VARIABLES")]    
+    [SerializeField] GameObject attackObject;
+    [SerializeField] float speedAttack;
+    float angleToRotate = 120;
+    Side sideAttack = Side.RIGHT;
+    bool canAttack = true;
+
+
+    #region UPDATE
+    void Update()
+    {
+        //////Detectamos el input
+        if (Input.GetButtonDown("Fire1") && canAttack)
+        {
+            switch (sideAttack)
+            {
+                case Side.RIGHT:
+                    {
+                        ////Instanciar el ataque
+                        Debug.Log("Right Attack");
+                        Destroy(Instantiate(attackObject,rightSpawner), speedAttack);
+                        attackObject.transform.parent = rightSpawner;
+
+                        ///rotar el brazo
+                        centreOfAttack.DORotate(new Vector3(0,  angleToRotate,0), speedAttack);
+
+                        ///coolDown
+                        canAttack = false;
+                        Invoke("RestartAttack", speedAttack);
+
+                        ///Cambiar brazo
+                        sideAttack = Side.LEFT;
+                        break;
+                    }
+                case Side.LEFT:
+                    {
+                        ////Instanciar el ataque
+                        Debug.Log("Left Attack");
+                        Destroy(Instantiate(attackObject, leftSpawner), speedAttack);
+                        attackObject.transform.parent = leftSpawner;
+
+                        ///rotar el brazo
+                        centreOfAttack.DORotate(new Vector3(0, -angleToRotate, 0), speedAttack);
+
+                        ///coolDown
+                        canAttack = false;
+                        Invoke("RestartAttack", speedAttack+0.2f);
+
+                        ///Cambiar brazo
+                        sideAttack = Side.RIGHT;
+                        break;
+                    }
+                default:
+                    {
+                        Debug.Log("ALGO NO FUNCIONA");
+                        break;
+                    }
+            }
+        }
+    }
+    #endregion
+
+    #region RESTART ATTACK
+    void RestartAttack()
+    {
+        canAttack = true;
+        centreOfAttack.Rotate(new Vector3(0, 0, 0));
+        
+    }
+    #endregion
+   
+
+}
